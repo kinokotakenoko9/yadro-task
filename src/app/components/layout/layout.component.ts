839ea175b871a1e03d8e2b5d8bcd53f70782240b
Chirkov Dmitri <chdmitry2@gmail.com>
2025-05-22 14:57:32 +0300

    add: search

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 @Component({
   selector: 'app-layout',
@@ -13,8 +18,19 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
     NzFlexModule,
     NzGridModule,
     RouterOutlet,
+    NzInputModule,
+    NzIconModule,
+    NzSpaceModule,
+    NzDropDownModule,
+    NzButtonModule,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  selectedDropdownItem: string = 'name';
+
+  onMenuItemClick(item: string): void {
+    this.selectedDropdownItem = item;
+  }
+}
